f1f5c66c87a4c5a0d74745f771d25126d86ec387
Frieler, Christopher <christopher@frieler.rocks>
2025-08-03 11:21:43 +0200

    Add KraftSQL's SNAPSHOT repository for internal dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,20 @@ allprojects {
 
     repositories {
         mavenCentral()
-        if ((version as String).endsWith("-SNAPSHOT")) {
+        if (version.toString().endsWith("-SNAPSHOT")) {
             mavenLocal()
+            maven("https://maven.pkg.github.com/KraftSQL/-") {
+                credentials { // Although the packages are public, GitHub's maven registry requires authentication:
+                    username = findProperty("github_packages_user")?.toString()
+                    password = findProperty("github_packages_password")?.toString()
+                }
+            }
+        }
+    }
+
+    configurations.all {
+        resolutionStrategy {
+            cacheChangingModulesFor(0, TimeUnit.SECONDS)
         }
     }
 }
