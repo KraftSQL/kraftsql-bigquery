4f4ffd1db40b3139948cb549c754117b5fe1ad5f
Frieler, Christopher <christopher@frieler.rocks>
2025-08-30 16:22:08 +0200

    Implement Delete command

---
+++ examples/src/main/kotlin/rocks/frieler/kraftsql/bq/examples/DeleteFood.kt
@@ -0,0 +1,38 @@
+package rocks.frieler.kraftsql.bq.examples
+
+import rocks.frieler.kraftsql.bq.examples.data.Category
+import rocks.frieler.kraftsql.bq.examples.data.Product
+import rocks.frieler.kraftsql.bq.examples.data.products
+import rocks.frieler.kraftsql.expressions.`=`
+import rocks.frieler.kraftsql.bq.ddl.create
+import rocks.frieler.kraftsql.bq.ddl.drop
+import rocks.frieler.kraftsql.bq.dml.Delete
+import rocks.frieler.kraftsql.bq.dml.execute
+import rocks.frieler.kraftsql.bq.dml.insertInto
+import rocks.frieler.kraftsql.bq.dql.execute
+import rocks.frieler.kraftsql.bq.dsl.Select
+import rocks.frieler.kraftsql.bq.expressions.Constant
+import rocks.frieler.kraftsql.bq.objects.Table
+import rocks.frieler.kraftsql.objects.DataRow
+import kotlin.collections.forEach
+
+fun main() {
+    try {
+        products.create()
+        Product(1, "Apple", Category(1, "Food")).insertInto(products)
+        Product(2, "Pants", Category(2, "Clothes")).insertInto(products)
+
+        deleteFood(products).also { println("Deleted $it products.") }
+
+        Select<DataRow> { from(products) }
+            .execute()
+            .forEach { println(it) }
+
+    } finally {
+        products.drop(ifExists = true)
+    }
+}
+
+fun deleteFood(productTable: Table<Product>) =
+    Delete(productTable, productTable[Product::category][Category::name] `=` Constant("Food"))
+        .execute()
---
+++ kraftsql-bigquery/src/main/kotlin/rocks/frieler/kraftsql/bq/dml/Delete.kt
@@ -0,0 +1,11 @@
+package rocks.frieler.kraftsql.bq.dml
+
+import rocks.frieler.kraftsql.bq.engine.BigQueryConnection
+import rocks.frieler.kraftsql.bq.engine.BigQueryEngine
+import rocks.frieler.kraftsql.bq.objects.Table
+import rocks.frieler.kraftsql.dml.execute
+import rocks.frieler.kraftsql.expressions.Expression
+
+class Delete(table: Table<*>, condition: Expression<BigQueryEngine, Boolean>) : rocks.frieler.kraftsql.dml.Delete<BigQueryEngine>(table, condition)
+
+fun Delete.execute() = execute(BigQueryConnection.Default.get())
