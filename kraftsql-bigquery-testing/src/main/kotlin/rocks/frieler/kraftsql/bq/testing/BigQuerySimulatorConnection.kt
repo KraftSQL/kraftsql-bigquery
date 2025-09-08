1d09eb58d7fc0fd2088dbfff6805a4d6a6bcb87f
Frieler, Christopher <christopher@frieler.rocks>
2025-09-08 15:03:34 +0200

    Implement REPLACE()

--- kraftsql-bigquery-testing/src/main/kotlin/rocks/frieler/kraftsql/bq/testing/BigQuerySimulatorConnection.kt
+++ kraftsql-bigquery-testing/src/main/kotlin/rocks/frieler/kraftsql/bq/testing/BigQuerySimulatorConnection.kt
@@ -1,6 +1,7 @@
 package rocks.frieler.kraftsql.bq.testing
 
 import rocks.frieler.kraftsql.bq.engine.BigQueryEngine
+import rocks.frieler.kraftsql.bq.expressions.Replace
 import rocks.frieler.kraftsql.expressions.Expression
 import rocks.frieler.kraftsql.objects.DataRow
 import rocks.frieler.kraftsql.testing.engine.SimulatorConnection
@@ -8,6 +9,17 @@ import rocks.frieler.kraftsql.testing.engine.SimulatorConnection
 class BigQuerySimulatorConnection : SimulatorConnection<BigQueryEngine>() {
     override fun <T> simulateExpression(expression: Expression<BigQueryEngine, T>) : (DataRow) -> T? =
         when (expression) {
+            is Replace -> { row ->
+                val originalValue = simulateExpression(expression.originalValue).invoke(row)
+                val fromPattern = simulateExpression(expression.fromPattern).invoke(row)!!
+                @Suppress("UNCHECKED_CAST")
+                if (fromPattern.isEmpty()) {
+                    originalValue
+                } else {
+                    val toPattern = simulateExpression(expression.toPattern).invoke(row)!!
+                    originalValue?.replace(fromPattern, toPattern)
+                } as T?
+            }
             else -> super.simulateExpression(expression)
         }
 }
