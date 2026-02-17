7dcf3f6f619e0cdcdcd09e79846dc2d87d12bb7f
Frieler, Christopher <christopher@frieler.rocks>
2026-02-17 21:30:16 +0100

    Reimplement keyword counting with UNNEST

--- examples/src/test/kotlin/rocks/frieler/kraftsql/bq/examples/ProductKeywordsTest.kt
+++ examples/src/test/kotlin/rocks/frieler/kraftsql/bq/examples/ProductKeywordsTest.kt
@@ -1,10 +1,7 @@
 package rocks.frieler.kraftsql.bq.examples
 
 import io.kotest.matchers.collections.shouldBeEmpty
-import io.kotest.matchers.collections.shouldContainAll
-import io.kotest.matchers.maps.shouldBeEmpty
-import io.kotest.matchers.maps.shouldContain
-import io.kotest.matchers.maps.shouldContainKeys
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
 import org.junit.jupiter.api.Test
 import rocks.frieler.kraftsql.bq.examples.data.Category
 import rocks.frieler.kraftsql.bq.examples.data.Product
@@ -18,16 +15,7 @@ class ProductKeywordsTest {
     private val food = Category(1, "Food")
 
     @Test
-    fun `collectProductKeywords() can handle empty data`() {
-        val products = ConstantData.empty<Product>()
-
-        val keywords = collectProductKeywords(products).collect()
-
-        keywords.shouldBeEmpty()
-    }
-
-    @Test
-    fun `collectProductKeywords() collects name, category and tags per Product`() {
+    fun `collectProductKeywords() counts name, category and tags of Products`() {
         val products = ConstantData(
             Product(1, "Chocolate", food, tags = arrayOf("sweets")),
             Product(2, "Lemon", food, tags = arrayOf("sour")),
@@ -35,44 +23,38 @@ class ProductKeywordsTest {
 
         val keywords = collectProductKeywords(products).collect()
 
-        keywords.shouldContainAll(
-            DataRow("keywords" to arrayOf("Chocolate", "Food", "sweets")),
-            DataRow("keywords" to arrayOf("Lemon", "Food", "sour")),
+        keywords shouldContainExactlyInAnyOrder listOf (
+            DataRow("keyword" to "Chocolate", "count" to 1L),
+            DataRow("keyword" to "Food", "count" to 2L),
+            DataRow("keyword" to "Lemon", "count" to 1L),
+            DataRow("keyword" to "sweets", "count" to 1L),
+            DataRow("keyword" to "sour", "count" to 1L),
         )
     }
 
     @Test
-    fun `countKeywords() can handle empty data`() {
-        val words = ConstantData.empty<DataRow>(listOf("keywords"))
+    fun `collectProductKeywords() can handle Product without tags`() {
+        val products = ConstantData(
+            Product(1, "Chocolate", food, tags = arrayOf("sweets")),
+            Product(2, "Lemon", food),
+        )
 
-        val wordCounts = countKeywords(words)
+        val keywords = collectProductKeywords(products).collect()
 
-        wordCounts.shouldBeEmpty()
+        keywords shouldContainExactlyInAnyOrder listOf (
+            DataRow("keyword" to "Chocolate", "count" to 1L),
+            DataRow("keyword" to "Food", "count" to 2L),
+            DataRow("keyword" to "Lemon", "count" to 1L),
+            DataRow("keyword" to "sweets", "count" to 1L),
+        )
     }
 
     @Test
-    fun `countKeywords() collects all words`() {
-        val words = ConstantData(
-            DataRow("keywords" to arrayOf("sweets")),
-            DataRow("keywords" to arrayOf("sour")),
-        )
+    fun `collectProductKeywords() can handle empty product data`() {
+        val products = ConstantData.empty<Product>()
 
-        val wordCounts = countKeywords(words)
+        val keywordCounts = collectProductKeywords(products).collect()
 
-        wordCounts.shouldContainKeys("sweets", "sour")
-    }
-
-    @Test
-    fun `countKeywords() counts occurrences per word`() {
-        val words = ConstantData(
-            DataRow("keywords" to arrayOf("fruit", "sweet")),
-            DataRow("keywords" to arrayOf("fruit", "sour")),
-        )
-
-        val wordCounts = countKeywords(words)
-
-        wordCounts.shouldContain("fruit", 2L)
-        wordCounts.shouldContain("sweet", 1L)
-        wordCounts.shouldContain("sour", 1L)
+        keywordCounts.shouldBeEmpty()
     }
 }
