943d06dc56abc8996eff104aa72afa8f903cfa24
Frieler, Christopher <christopher@frieler.rocks>
2026-05-14 14:36:55 +0200

    Implement BigQuery behavior for naming result columns without alias

--- kraftsql-bigquery-testing/src/main/kotlin/rocks/frieler/kraftsql/bq/testing/simulator/engine/BigQueryQueryEvaluator.kt
+++ kraftsql-bigquery-testing/src/main/kotlin/rocks/frieler/kraftsql/bq/testing/simulator/engine/BigQueryQueryEvaluator.kt
@@ -5,6 +5,8 @@ import rocks.frieler.kraftsql.bq.expressions.Constant
 import rocks.frieler.kraftsql.bq.testing.simulator.expressions.BigQueryExpressionEvaluator
 import rocks.frieler.kraftsql.bq.testing.simulator.expressions.BigQuerySubexpressionCollector
 import rocks.frieler.kraftsql.dql.Select
+import rocks.frieler.kraftsql.expressions.Column
+import rocks.frieler.kraftsql.expressions.Expression
 import rocks.frieler.kraftsql.objects.DataRow
 import rocks.frieler.kraftsql.testing.simulator.engine.EngineState
 import rocks.frieler.kraftsql.testing.simulator.engine.GenericQueryEvaluator
@@ -19,9 +21,40 @@ object BigQueryQueryEvaluator : GenericQueryEvaluator<BigQueryEngine>(
         correlatedJoinsEnabled = true
     }
 
+    private val internalGeneratedNamePattern = Regex($$"^\\$col\\d+$")
+
+    context(activeState: EngineState<BigQueryEngine>)
+    override fun selectRows(select: Select<BigQueryEngine, *>): List<DataRow> =
+        fillPublicColumnNames(super.selectRows(select))
+
+    private fun fillPublicColumnNames(rows: List<DataRow>): List<DataRow> {
+        val givenColumnNames = rows.firstOrNull()?.columnNames ?: emptyList()
+        val takenColumnNames = givenColumnNames.filterNot { it.matches(internalGeneratedNamePattern) }.toMutableList()
+        var currentNumberForGeneratedNames = 0
+        val generatedNames = mutableMapOf<String, String>()
+        givenColumnNames.filter { name -> name.matches(internalGeneratedNamePattern) }.forEach { name ->
+            while (takenColumnNames.contains("f${currentNumberForGeneratedNames}_")) currentNumberForGeneratedNames++
+            generatedNames[name] = "f${currentNumberForGeneratedNames}_".also { takenColumnNames.add(it) }
+        }
+
+        return rows.map { row ->
+            DataRow(row.entries.map { (name, value) -> (generatedNames.getOrDefault(name, name)) to value })
+        }
+    }
+
     context(activeState: EngineState<BigQueryEngine>)
     override fun evaluateSelectInternal(select: Select<BigQueryEngine, *>, correlatedData: DataRow?): List<DataRow> {
         if (select.grouping.any { it is Constant }) { throw SQLException("Cannot GROUP BY literal values") }
         return super.evaluateSelectInternal(select, correlatedData)
+    }
+
+    override fun fillColumnNames(columns: List<Pair<String?, Expression<BigQueryEngine, *>>>): List<Pair<String, Expression<BigQueryEngine, *>>> {
+        var currentNumberForGeneratedNames = 1
+        return columns.map { (name, expression) ->
+            val effectiveName = name
+                ?: (expression as? Column)?.name?.substringAfterLast(".")
+                ?: $$"$col$${currentNumberForGeneratedNames++}"
+            effectiveName to expression
+        }
     }
 }
